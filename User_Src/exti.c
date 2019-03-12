85defbd4f2ebee00ddc657f40638398cf3e9701a
Saoskywalker <773917760@qq.com>
2019-03-12 17:52:11 +0800

    2019.3.12

--- User_Src/exti.c
+++ User_Src/exti.c
@@ -1,6 +1,4 @@
 #include "exti.h"
-#include "delay.h"
-#include "AppLib.h"
 
 //////////////////////////////////////////////////////////////////////////////////	 
 //本程序只供学习使用，未经作者许可，不得用于其它任何用途
@@ -20,41 +18,86 @@
 //初始化PA0,PA13,PA15为中断输入.
 void EXTIX_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStructure;
+ 	GPIO_InitTypeDef GPIO_InitStructure;
  	EXTI_InitTypeDef EXTI_InitStructure;
  	NVIC_InitTypeDef NVIC_InitStructure;
-
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO|RCC_APB2Periph_GPIOB,ENABLE);	//使能IO&复用功能时钟
 	
-	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_14;
+	//int pin input
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);	 
+	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_3|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; //设置成上拉输入
  	GPIO_Init(GPIOB, &GPIO_InitStructure);
 	
-  //GPIOB.14 中断线以及中断初始化配置   下降沿触发
-  	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB,GPIO_PinSource14);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);	//使能IO&复用功能时钟
 
-  	EXTI_InitStructure.EXTI_Line = EXTI_Line14;
+  //中断线以及中断初始化配置   下降沿触发
+  	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB,GPIO_PinSource3);
+
+  	EXTI_InitStructure.EXTI_Line = EXTI_Line3;
   	EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;	
   	EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
   	EXTI_InitStructure.EXTI_LineCmd = ENABLE;
   	EXTI_Init(&EXTI_InitStructure);	 	//根据EXTI_InitStruct中指定的参数初始化外设EXTI寄存器
 
-  	NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;			//使能按键所在的外部中断通道
-  	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x02;	//抢占优先级2， 
-  	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x03;					//子优先级3
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource12);
+	EXTI_InitStructure.EXTI_Line = EXTI_Line12;
+	EXTI_Init(&EXTI_InitStructure); //根据EXTI_InitStruct中指定的参数初始化外设EXTI寄存器
+
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource13);
+	EXTI_InitStructure.EXTI_Line = EXTI_Line13;
+	EXTI_Init(&EXTI_InitStructure); //根据EXTI_InitStruct中指定的参数初始化外设EXTI寄存器
+
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource14);
+	EXTI_InitStructure.EXTI_Line = EXTI_Line14;
+	EXTI_Init(&EXTI_InitStructure); //根据EXTI_InitStruct中指定的参数初始化外设EXTI寄存器
+
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource15);
+	EXTI_InitStructure.EXTI_Line = EXTI_Line15;
+	EXTI_Init(&EXTI_InitStructure); //根据EXTI_InitStruct中指定的参数初始化外设EXTI寄存器
+
+	NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;			//使能按键所在的外部中断通道
+  	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3;	//抢占优先级3， 最低
+  	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;					//子优先级3, 最低
   	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;								//使能外部中断通道
   	NVIC_Init(&NVIC_InitStructure); 	//根据NVIC_InitStruct中指定的参数初始化外设NVIC寄存器
 }
 
-//外部中断14服务程序 
+//外部中断3
+void EXTI3_IRQHandler(void)
+{
+	if (EXTI_GetITStatus(EXTI_Line3) != RESET)
+	{
+
+		EXTI_ClearITPendingBit(EXTI_Line3); //清除线路挂起位
+	}
+
+}
+
+//外部中断10~15服务程序 
 void EXTI15_10_IRQHandler(void)
 {
-	if(EXTI_GetITStatus(EXTI_Line14)!=RESET)
+	if (EXTI_GetITStatus(EXTI_Line12) != RESET)
 	{
-		delay_ms(1);
-		if(INPUT14_PIN==0)			
-			HallCount++;
-		EXTI_ClearITPendingBit(EXTI_Line14);  //清除线路挂起位
+
+		EXTI_ClearITPendingBit(EXTI_Line12); //清除线路挂起位
+	}
+
+	if (EXTI_GetITStatus(EXTI_Line13) != RESET)
+	{
+
+		EXTI_ClearITPendingBit(EXTI_Line13); //清除线路挂起位
+	}
+
+	if (EXTI_GetITStatus(EXTI_Line14) != RESET)
+	{
+
+		EXTI_ClearITPendingBit(EXTI_Line14); //清除线路挂起位
+	}
+
+	if (EXTI_GetITStatus(EXTI_Line15) != RESET)
+	{
+
+		EXTI_ClearITPendingBit(EXTI_Line15); //清除线路挂起位
 	}
 }
  
