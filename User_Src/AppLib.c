ecba68359e9cdb66f0c349c8adcf5ab99439b33e
Saoskywalker <773917760@qq.com>
2019-04-15 22:15:35 +0800

    2019.4.15

--- User_Src/AppLib.c
+++ User_Src/AppLib.c
@@ -205,6 +205,75 @@ void MiddleMotorStop(void)
 	TIM_SetCompare4(TIM1, 0);
 }
 
+//传送数据给匿名四轴上位机软件(V2.6版本)
+//fun:功能字. 0XA0~0XAF
+//data:数据缓存区,最多28字节!!
+//len:data区有效数据个数
+void usart1_niming_report(u8 fun,u8*data,u8 len)
+{
+	u8 send_buf[32];
+	u8 i;
+	if(len>28)return;	//最多28字节数据 
+	send_buf[len+3]=0;	//校验数置零
+	send_buf[0]=0X88;	//帧头
+	send_buf[1]=fun;	//功能字
+	send_buf[2]=len;	//数据长度
+	for(i=0;i<len;i++) send_buf[3+i]=data[i];			//复制数据
+	for(i=0;i<len+3;i++) send_buf[len+3]+=send_buf[i];	//计算校验和	
+	for(i=0;i<len+4;i++) uasrt1SendByte(send_buf[i]);	//发送数据到串口1 
+}
+//发送加速度传感器数据和陀螺仪数据
+//aacx,aacy,aacz:x,y,z三个方向上面的加速度值
+//gyrox,gyroy,gyroz:x,y,z三个方向上面的陀螺仪值
+void mpu6050_send_data(short aacx,short aacy,short aacz,short gyrox,short gyroy,short gyroz)
+{
+	u8 tbuf[12]; 
+	tbuf[0]=(aacx>>8)&0XFF;
+	tbuf[1]=aacx&0XFF;
+	tbuf[2]=(aacy>>8)&0XFF;
+	tbuf[3]=aacy&0XFF;
+	tbuf[4]=(aacz>>8)&0XFF;
+	tbuf[5]=aacz&0XFF; 
+	tbuf[6]=(gyrox>>8)&0XFF;
+	tbuf[7]=gyrox&0XFF;
+	tbuf[8]=(gyroy>>8)&0XFF;
+	tbuf[9]=gyroy&0XFF;
+	tbuf[10]=(gyroz>>8)&0XFF;
+	tbuf[11]=gyroz&0XFF;
+	usart1_niming_report(0XA1,tbuf,12);//自定义帧,0XA1
+}	
+//通过串口1上报结算后的姿态数据给电脑
+//aacx,aacy,aacz:x,y,z三个方向上面的加速度值
+//gyrox,gyroy,gyroz:x,y,z三个方向上面的陀螺仪值
+//roll:横滚角.单位0.01度。 -18000 -> 18000 对应 -180.00  ->  180.00度
+//pitch:俯仰角.单位 0.01度。-9000 - 9000 对应 -90.00 -> 90.00 度
+//yaw:航向角.单位为0.1度 0 -> 3600  对应 0 -> 360.0度
+void usart1_report_imu(short aacx,short aacy,short aacz,short gyrox,short gyroy,short gyroz,short roll,short pitch,short yaw)
+{
+	u8 tbuf[28]; 
+	u8 i;
+	for(i=0;i<28;i++)tbuf[i]=0;//清0
+	tbuf[0]=(aacx>>8)&0XFF;
+	tbuf[1]=aacx&0XFF;
+	tbuf[2]=(aacy>>8)&0XFF;
+	tbuf[3]=aacy&0XFF;
+	tbuf[4]=(aacz>>8)&0XFF;
+	tbuf[5]=aacz&0XFF; 
+	tbuf[6]=(gyrox>>8)&0XFF;
+	tbuf[7]=gyrox&0XFF;
+	tbuf[8]=(gyroy>>8)&0XFF;
+	tbuf[9]=gyroy&0XFF;
+	tbuf[10]=(gyroz>>8)&0XFF;
+	tbuf[11]=gyroz&0XFF;	
+	tbuf[18]=(roll>>8)&0XFF;
+	tbuf[19]=roll&0XFF;
+	tbuf[20]=(pitch>>8)&0XFF;
+	tbuf[21]=pitch&0XFF;
+	tbuf[22]=(yaw>>8)&0XFF;
+	tbuf[23]=yaw&0XFF;
+	usart1_niming_report(0XAF,tbuf,28);//飞控显示帧,0XAF
+}  
+
 void Interaction(void)
 {
 	RGB_Renew(0, 20, 160);
@@ -252,7 +321,7 @@ void Interaction(void)
 
 void TimeAction(void)
 {
-	u8 j, k = 0;
+	u8 j = 0;
 
 	RTCAlarm = 0;
 
@@ -348,6 +417,26 @@ void TimeAction(void)
 
 void RemindAction(void)
 {
+	u8 temperature;  	    
+	u8 humidity;  
+	// float pitch,roll,yaw; 		//欧拉角
+	short aacx,aacy,aacz;		//加速度传感器原始数据
+	short gyrox,gyroy,gyroz;	//陀螺仪原始数据
+	// short temp;					//温度	
+
+	DHT11_Read_Data(&temperature,&humidity);	//读取温湿度值
+	printf("Temp:%d; Hum:%d\n", temperature, humidity);
+
+	// temp = MPU_Get_Temperature();	//得到温度值
+	//mpu_dmp_get_data(&pitch,&roll,&yaw); //运算姿态获取
+	MPU_Get_Accelerometer(&aacx,&aacy,&aacz);	//得到加速度传感器数据
+	MPU_Get_Gyroscope(&gyrox,&gyroy,&gyroz);	//得到陀螺仪数据
+	//用自定义帧发送加速度和陀螺仪原始数据
+	mpu6050_send_data(aacx,aacy,aacz,gyrox,gyroy,gyroz);
+	//发送运算姿态
+	// usart1_report_imu(aacx,aacy,aacz,gyrox,gyroy,
+	// 				gyroz,(int)(roll*100),(int)(pitch*100),(int)(yaw*10));
+	
 	MusicStart(V_HELLO);
 }
 
